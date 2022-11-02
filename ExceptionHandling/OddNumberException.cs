3bdb3070477b6e44e435baf07b30de4617f78e0f
bayuhyuuga <radamanthys_ok@yahoo.com>
2022-11-02 12:50:52 +0700

    Add project files.

---
+++ ExceptionHandling/OddNumberException.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class OddNumberException: Exception
+    {
+        public OddNumberException()
+        {
+
+        }
+
+        public OddNumberException(string message): base(message)
+        {
+
+        }
+
+        public OddNumberException(string message, Exception inner): base(message, inner)
+        {
+
+        }
+
+        //public override string Message
+        //{
+        //    get
+        //    {
+        //        return "Divisor can not be odd number!";
+        //    }
+        //}
+
+        public override string HelpLink 
+        {
+            get
+            {
+                return "Get info call Han Han Hanafi https://www.google.com/";
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,65 @@
+﻿using ExceptionHandling;
+using System.Text;
+
+namespace ErrorHandling
+{
+    class Program
+    {
+        static void Main()
+        {
+            // outer try
+            try
+            {
+                // inner try
+                try
+                {
+                    int a, b, c;
+                    Console.Write("Nilai A : ");
+                    a = int.Parse(Console.ReadLine());
+
+                    Console.Write("Nilai B : ");
+                    b = int.Parse(Console.ReadLine());
+
+                    if (b % 2 > 0)
+                    {
+                        throw new OddNumberException("Odd Number occured inside static main method");
+                    }
+
+                    c = a / b;
+                    Console.WriteLine($"Result : {c}");
+                }
+                catch (OddNumberException one)
+                {
+                    Console.WriteLine($"Message : {one.Message}");
+                    Console.WriteLine($"Helplink : {one.HelpLink}");
+                }
+                catch (Exception ex)
+                {
+                    string filePath = @"C:\Error\error.txt";
+                    if (File.Exists(filePath))
+                    {
+                        StringBuilder sbr = new StringBuilder();
+                        sbr.Append($"Message : {ex.Message}");
+                        sbr.Append($"Source : {ex.Source}");
+
+                        StreamWriter stw = new StreamWriter(filePath);
+                        stw.Write(sbr.ToString());
+                        stw.Close();
+                        Console.WriteLine("An Error has occured! Please try again later");
+                    }
+                    else
+                    {
+                        string Message = filePath + " Does not exist!";
+                        throw new FileNotFoundException(Message, ex);
+                    } 
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error details!");
+                Console.WriteLine($"Inner exception Message : {ex.InnerException.Message}");
+                Console.WriteLine($"Inner exception Source : {ex.InnerException.Source}");
+            }
+        }
+    }
+}
